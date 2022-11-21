63b1e003730959c557758720eb3bce1eb82753b0
nikita <beskhlebnovnikia@gmail.com>
2022-11-21 20:09:30 +0300

    Create len

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 New_array = []
 Final_array = []
-Element = input("Enter word")
+Element = input(" Enter word. If you want end the array write 'stop'")
 while Element != "stop":
     New_array.append(Element)
-    Element = input("Enter word")
+    Element = input(" Enter word ")
+for i in New_array:
+    SymboLenght = len()
