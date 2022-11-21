792abf9b14a8e2d576833167542f52dea48d52d3
nikita <beskhlebnovnikia@gmail.com>
2022-11-21 20:17:22 +0300

    Added to finale element which that have less than 3 symbols

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 New_array = []
 Final_array = []
-Element = input(" Enter word. If you want end the array write 'stop'")
+Element = input(" Enter word. If you want end the array write 'stop' ")
 while Element != "stop":
     New_array.append(Element)
     Element = input(" Enter word ")
 for i in New_array:
-    SymboLenght = len()
+    SymboLenght = len(i)
+    if SymboLenght <= 3:
+        Final_array.append(i)
+print(Final_array)
